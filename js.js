fbd207fb97c056cf772e26c493fb566693626c5d
Karina <karina.kvych@gmail.com>
2023-05-14 12:31:37 +0100

    add forecast data

--- js.js
+++ js.js
@@ -1,12 +1,5 @@
 // Feature #1
-function getTime(){
-let currentTime = document.querySelector("#time-string");
-let now = new Date();
-
-let hours = now.getHours() > 9 ? now.getHours() : "0" + now.getHours();
-let minutes = now.getMinutes() > 9 ? now.getMinutes() : "0" + now.getMinutes();
-
-let days = [
+const days = [
   "Sunday",
   "Monday",
   "Tuesday",
@@ -15,12 +8,20 @@ let days = [
   "Friday",
   "Saturday",
 ];
-let day = days[now.getDay()];
+function getTime() {
+  let currentTime = document.querySelector("#time-string");
+  let now = new Date();
 
-currentTime.innerHTML = `${day} ${hours}:${minutes}`;
+  let hours = now.getHours() > 9 ? now.getHours() : "0" + now.getHours();
+  let minutes =
+    now.getMinutes() > 9 ? now.getMinutes() : "0" + now.getMinutes();
+
+  let day = days[now.getDay()];
+
+  currentTime.innerHTML = `${day} ${hours}:${minutes}`;
 }
 
-setInterval(getTime, 1000)
+setInterval(getTime, 1000);
 
 // Feature#2
 // function searchForCity(event) {
@@ -60,44 +61,80 @@ setInterval(getTime, 1000)
 //   handlTemperature(55, "farengate");
 // });
 
-
-
-
 // Current temperature
 const apiKey = `d0bec9d6480b2df7e1b8e4642f141337`;
 
+function forecast_data(response) {
+  console.log("forecast_data",response.data.list[0])
+  const list = document.querySelector(".list")
 
+  for (const forecast of response.data.list){
+    const date = new Date(forecast.dt_txt)
+    let hours = date.getHours() > 9 ? date.getHours() : "0" + date.getHours();
+    let minutes =
+      date.getMinutes() > 9 ? date.getMinutes() : "0" + date.getMinutes();
+      
+    const day = document.createElement("span")
+    day.innerText = `${date.getDate()} ${days[date.getDay()]} ${hours}:${minutes}`
+
+    const img = document.createElement("img")
+    img.src=`https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`
+    img.id = "currentIcon"
+    const p = document.createElement("p")
+    p.classList = `m-0`
+    p.innerText = `${Math.round(forecast.main.temp)}°C`
+
+
+
+    
+    const forDay = document.createElement("div")
+    forDay.id = "forDay"
+
+    forDay.appendChild(day)
+    forDay.appendChild(img)
+    forDay.appendChild(p)
+
+    list.appendChild(forDay)
+  }
+
+}
 function showTemperature(response) {
-  console.log(response.data);
+  console.log('res ',response.data);
+  const forUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${response.data.coord.lat}&lon=${response.data.coord.lon}&appid=${apiKey}&units=metric`;
+  axios.get(forUrl).then(forecast_data);
+
   let showCurrentCity = document.querySelector("#cityName");
-  showCurrentCity.innerHTML = `${response.data.name}`
-  
+  showCurrentCity.innerHTML = `${response.data.name}`;
+
   let tempNow = document.querySelector("#tempNow");
-  tempNow.innerHTML = `${Math.round(response.data.main.temp)}`
-  
-  let weatherDescription = document.querySelector("#weatherDescription")
-  weatherDescription.innerHTML = `${response.data.weather[0].description}`
-  
+  tempNow.innerHTML = `${Math.round(response.data.main.temp)}`;
+
+  let weatherDescription = document.querySelector("#weatherDescription");
+  weatherDescription.innerHTML = `${response.data.weather[0].description}`;
+
   let weatherSituation = document.querySelector("#weatherSituation");
-  weatherSituation.innerHTML =`Humidity:${response.data.main.humidity}% <br/>
-  Wind speed: ${response.data.wind.speed} mph`
-  
+  weatherSituation.innerHTML = `Humidity:${response.data.main.humidity}% <br/>
+  Wind speed: ${response.data.wind.speed} mph`;
+
+  // let forecastData = document.querySelector("#forDay");
+  // forecastData.innerHTML = ``;
+
   const currentIcon = document.querySelector("#currentIcon");
-  const iconUrl = ` https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  const iconUrl = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
   currentIcon.src = iconUrl;
 }
 // Search
 
-function cityLocation(event){
-  event.preventDefault()
-  const cityInputTag = document.querySelector("#type-city")
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputTag.value}&appid=${apiKey}&units=metric`
-  axios.get(apiUrl).then(showTemperature)
+
+function cityLocation(event) {
+  event.preventDefault();
+  const cityInputTag = document.querySelector("#type-city");
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputTag.value}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
 }
 
-
 const searchCity = document.querySelector("#search-form");
-searchCity.addEventListener("submit", cityLocation)
+searchCity.addEventListener("submit", cityLocation);
 
 function myCurrentTemperature(position) {
   console.log(position);
@@ -105,24 +142,24 @@ function myCurrentTemperature(position) {
   let lon = position.coords.longitude;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
+
 }
 // Fetch via geolocation
-function showCity(){
+function showCity() {
   navigator.geolocation.getCurrentPosition(myCurrentTemperature);
 }
 
-  
 const currentTemperature = document.querySelector("#currentTemperature");
 currentTemperature.addEventListener("click", showCity);
 
 // For Header List
-function searchByHeader(event){
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${event.target.innerText}&appid=${apiKey}&units=metric`
-  axios.get(apiUrl).then(showTemperature)
+function searchByHeader(event) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${event.target.innerText}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
 }
 
 const headerList = document.querySelectorAll("#headerList li");
 
-for (let i=0; i<headerList.length; i++){
-  headerList[i].addEventListener("click", searchByHeader)
+for (let i = 0; i < headerList.length; i++) {
+  headerList[i].addEventListener("click", searchByHeader);
 }
